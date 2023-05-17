517136a497b3a63bb087c82047bcfc8e80b4bc3f
Usman F <usman_farzand@outlook.com>
2023-05-17 09:26:03 +0500

    fix

--- product_barcode_space/__manifest__.py
+++ product_barcode_space/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Product Barcode Generator',
-    'version': '14.0.3.2.0',
+    'version': '14.0.3.3.0',
     'summary': 'Generates EAN13 Standard Barcode for Product.',
     'live_test_url': 'https://www.youtube.com/watch?v=0BrFcOEkWu4&feature=youtu.be',
     'category': 'Inventory',
